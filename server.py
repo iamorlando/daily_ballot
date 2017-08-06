20861ac7b095747c080938988aaeab86a871e5ca
Orlando <iamorlando@gmail.com>
2017-08-06 13:33:29 -0400

    adding support for google app engine

--- server.py
+++ server.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  3 14:59:48 2017
+
+@author: iamorlando
+"""
+
+
 from flask import Flask
 from flask import request
 from ballot_api import get_representatives
 from flask import Response
+
+
 
 app = Flask(__name__)
 
@@ -17,4 +28,4 @@ def representatives():
     return response
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='127.0.0.1', port=8080)
